1da8575ed785c137592a2e0e188bb14454593f00
phasefactor <56488589+phasefactor@users.noreply.github.com>
2023-10-17 18:51:40 -0500

    Add first ~200 advertising and analytics sites form the whotracks.me trackers list.

--- list2json.py
+++ list2json.py
@@ -1,5 +1,10 @@
+#
+# first injest the input list
+#
+
 hostsFile = open("./input.txt", "r")
 
+# using set to avoid duplicates
 domains = set()
 
 for line in hostsFile:
@@ -14,27 +19,33 @@ for line in hostsFile:
     if len(line) == 0:
         continue
     
+    # NOTE - this escapes all .'s but does NOT check for them being part of a regex etc
     domains.add(line.replace(".", "\\\\."))
-
 
 
 hostsFile.close()
 
-count = 0
 
+#
+# write out the JSON
+#
+
+count = 0
 output = open("output.txt", "w")
 
+# open the rules array
 output.writelines("[\r")
 
+# change to list mostly to make it easy to check for the last item
 domains = list(domains)
 
-
-
+# loop through the list of domains
 for domain in domains:
     count += 1
     
-    s = "{ \"id\":" + str(count) + ",\"priority\":1,\"action\":{\"type\":\"block\"},\"condition\":{\"regexFilter\":\"http(s)?://(.*\\\\.)?" + domain + ".*\",}}"
+    s = "{ \"id\":"+str(count)+",\"priority\":1,\"action\":{\"type\":\"block\"},\"condition\":{\"regexFilter\":\"http(s)?://(.*\\\\.)?"+domain+".*\",}}"
     
+    # leave off the last comma
     if domain != domains[-1]:
         s += ","
 
@@ -43,7 +54,7 @@ for domain in domains:
     output.writelines(s)
 
 
-
+# close the rules array
 output.writelines("]")
 
 output.close()
